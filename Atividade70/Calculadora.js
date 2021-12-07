357cf108163383877b10ba3af9222be63b1f33c8
Renata Coraiola <rehcoraiola@outlook.com>
2021-12-07 10:44:01 -0300

    JS com Class, Metodo, Function

---
+++ Atividade70/Calculadora.js
@@ -0,0 +1,17 @@
+function somar(n1,n2){
+    return n1+n2
+}
+function subtrair(n1,n2){
+    return n1-n2
+}
+function multiplicar(n1,n2){
+    return n1*n2
+}
+function dividir(n1,n2){
+    return n1/n2
+}
+
+console.log(somar(2,4))
+console.log(subtrair(2,4))
+console.log(multiplicar(2,4))
+console.log(dividir(2,4))
---
+++ Atividade70/ClientePessoa.js
@@ -0,0 +1,11 @@
+class Pessoa{
+    nome;
+    rg;
+}
+class Cliente{
+    numeroConta;
+}
+
+console.log(p1=new Pessoa)
+console.log(c1=new Cliente)
+
