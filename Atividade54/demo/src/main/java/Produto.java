95d4f789bf9362aa832ecf4f41255dc538b05842
Renata Coraiola <rehcoraiola@outlook.com>
2021-12-07 10:34:53 -0300

    Java WEB e parâmetros em requisições com classe PrintWriter.

---
+++ Atividade54/demo/src/main/java/Produto.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/produto")
+public class Produto extends HttpServlet{
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out=resp.getWriter();
+        
+        String nome = req.getParameter("name");
+        String valor= req.getParameter("valor");
+        String idCategoria = req.getParameter("idCat");
+
+        if(valor != null && idCategoria != null){
+            float vl= Float.parseFloat(valor);
+            int id= Integer.parseInt(idCategoria);
+            out.printf("Servlet Produto \t %s  R$: %.2f \t Id: %d",nome,vl,id);
+        }else{
+            out.printf("Servlet Produto \t %s",nome);
+        }
+    }
+}
---
+++ Atividade54/demo/target/demo-1.0-SNAPSHOT/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Atividade 53 - Servelet</h2>
+<a href="/demo-1/categoria">Categoria</a><br/>
+<a href="/demo-1/produto">Produto</a>
+</body>
+</html>
